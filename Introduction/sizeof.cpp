2b7d74fd3e3def9d94f76fb0c5e3064dc090ffa2
Arvind Kharwal <karvind629@gmail.com>
2026-07-29 10:30:02 +0530

    29 July 2026

---
+++ Introduction/sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long double d;
+    cout << sizeof(int) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(long) << endl;
+    cout << sizeof(d) << endl;
+}
