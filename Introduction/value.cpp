eed6d5e0ea839c4155543686b0a66e39d8d348f4
Arvind Kharwal <karvind629@gmail.com>
2026-07-29 09:57:32 +0530

    29 July 2026

---
+++ Introduction/value.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int sum(int, int); // function declaration
+int main()
+{
+    int a, b, r;
+    cout << "Enter two numbers:";
+    cin >> a >> b;
+    r = sum(a, b);
+    cout << r;
+}
+int sum(int x, int y)
+{
+    int s;
+    s = x + y;
+    return (s);
+}
