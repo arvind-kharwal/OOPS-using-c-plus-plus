5875dfec55a5fd78e03f507d4997b534379a82a2
Arvind Kharwal <karvind629@gmail.com>
2026-08-12 08:57:45 +0530

    12 August 2026

--- Variadic/default.cpp
+++ Variadic/default.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
-void display(int x = 10)
+void display(int z, int x = 50)
 {
-    cout << x << endl;
+    cout << z << x << endl;
 }
 int main()
 {
-    display();
+    display(10);
+    display(20, 40);
 }
