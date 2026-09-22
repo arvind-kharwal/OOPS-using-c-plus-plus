71f128bbc48e03fa80fdc062ba41674820c4a684
Arvind Kharwal <karvind629@gmail.com>
2026-09-22 09:12:42 +0530

    22 September 2026

--- Constructor/First1.cpp
+++ Constructor/First1.cpp
@@ -2,28 +2,36 @@
 using namespace std;
 class Student
 {
+    int roll;
     string name;
     static int count;
 
 public:
-    Student(string);
+    Student(int, string);
     void display();
+    static void showstd();
 };
-Student::Student(string n)
+Student::Student(int r, string n)
 {
+    roll = r;
     name = n;
     count++;
 }
 void Student::display()
 {
+    cout << roll << endl;
     cout << name << endl;
-    cout << count << endl;
+}
+void Student::showstd()
+{
+    cout << "The total number of students are: " << count << endl;
 }
 int Student::count;
 int main()
 {
-    Student S1("Arvind");
+    Student S1(2, "Arvind");
     S1.display();
-    Student S2 = Student("Yuvaan");
+    Student S2 = Student(10, "Yuvaan");
     S2.display();
+    Student::showstd();
 }
