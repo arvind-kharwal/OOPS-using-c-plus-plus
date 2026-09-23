20ec008b1636c923100e663cb79492221b5800ca
Arvind Kharwal <karvind629@gmail.com>
2026-09-23 09:48:21 +0530

    23 September 2026

---
+++ Constructor/program3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Example
+{
+    int a, b;
+
+public:
+    Example(int, int);
+    Example(Example &);
+    void display();
+};
+Example::Example(int x, int y)
+{
+    a = x;
+    b = y;
+}
+Example::Example(Example &E)
+{
+    a = E.a;
+    b = E.b;
+}
+void Example::display()
+{
+    cout << a << b << endl;
+}
+
+int main()
+{
+    Example E1(10, 20);
+    E1.display();
+    Example E2(E1);
+    E2.display();
+}
